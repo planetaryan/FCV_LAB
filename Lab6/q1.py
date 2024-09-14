5deaa44fa6ca87f17228547be19e619cce1ba745
planetaryan <117113732+planetaryan@users.noreply.github.com>
2024-09-14 16:19:26 +0530

    Update q1.py

--- Lab6/q1.py
+++ Lab6/q1.py
@@ -30,15 +30,33 @@ def visualize_keypoints(image, keypoints, title='Keypoints'):
     plt.axis('off')
     plt.show()
 
+def visualize_matches(image1, keypoints1, image2, keypoints2, matches):
+    img_matches = cv2.drawMatches(
+        image1, keypoints1, image2, keypoints2,
+        matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+    )
+    img_matches_rgb = cv2.cvtColor(img_matches, cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(12, 8))
+    plt.imshow(img_matches_rgb)
+    plt.title('Matching Keypoints')
+    plt.axis('off')
+    plt.show()
+
 if __name__ == "__main__":
     image1 = cv2.imread('images/tower1.jpg', cv2.IMREAD_GRAYSCALE)
     image2 = cv2.imread('images/tower2.jpg', cv2.IMREAD_GRAYSCALE)
     keypoints1, desc1 = sift_custom(image1)
     keypoints2, desc2 = sift_custom(image2)
+
     if desc1.size == 0 or desc2.size == 0:
         print("No descriptors found in one of the images.")
     else:
         matches = nearest_neighbour_matching(desc1, desc2)
+
+        # Create a list of DMatch objects required by cv2.drawMatches
+        good_matches = [cv2.DMatch(i, matches[i], 0) for i in range(len(matches))]
+
         print("Descriptor matches:", matches)
         visualize_keypoints(image1, keypoints1, 'Keypoints in Image 1')
         visualize_keypoints(image2, keypoints2, 'Keypoints in Image 2')
+        visualize_matches(image1, keypoints1, image2, keypoints2, good_matches)
